a4afe7b56e05fc816cf18e43ad08d2cdd69d94e3
Justus Ojala <justus.ojala@aalto.fi>
2025-04-04 11:01:42 +0300

    Added '::' host

--- app.ts
+++ app.ts
@@ -58,6 +58,6 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
   });
 
-app.listen(PORT, () => {
+app.listen(PORT, '::', () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
